7c42643f4f96f1ea10feb7f948bd5409058cce5b
Arkadiy <arkady.krylosov@yandex.ru>
2021-07-02 00:16:32 +0300

    Update CrudConsole.java

--- src/main/java/CrudConsole.java
+++ src/main/java/CrudConsole.java
@@ -1,18 +1,5 @@
-import controller.LabelController;
-import controller.PostController;
-import controller.WriterController;
-import model.Label;
-import model.Post;
-import model.Writer;
-import repository.Implementation.JsonLabelRepositoryImpl;
-import repository.Implementation.JsonPostRepositoryImpl;
-import repository.Implementation.JsonWriterRepositoryImpl;
-import utility.Utility;
-import view.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import view.*;
 
 public class CrudConsole {
     public static void main(String[] args) {
